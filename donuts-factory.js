cca3b85ab815ef049ca70e9065a7e7befe5c1024
mohammed-husban <22015295011@std.bau.edu.jo>
2024-06-04 20:45:20 +0300

    edit 2

--- donuts-factory.js
+++ donuts-factory.js
@@ -1,125 +1,128 @@
 ///////////////////////////////////////////////////////////////////////////////////////// ( EX1 )
 
-
-let name = prompt(" Please enter your name : ");
-let Gender = prompt(" Please enter your gender (male or female) : ");
-if (Gender === " male") {
-    alert(" Hello mr "+ name)
-}
-else if ( Gender === "female") {
-    alert(" Hello ms "+ name)
-}
-    else {
-alert(" Welcome " +name);
-    }
-let orderType = prompt(" Would you like to order Donut, Coffee, Ice cream, or Bakery ");
-let orderName = prompt(" Please write the name of the order : ");
-alert("Your order is getting prepared  " +name);
-console.log(" You Have one Customer " + name + " has ordered " + orderType + " .");
-
-
-///////////////////////////////////////////////////////////////////////////////////////// ( 1 )
-
-
-// console.log(-5*3);
-// console.log("JavaScript " + 50  );
-// console.log (17 % 5) ;
-// console.log (5 % 17 )  ;
-// console.log (5/10 )  ;
-// console.log (4 === '4') ;
-// console.log (4 != 5)  ;
-// console.log (7 <= 8) ;
-// console.log ("Hello" + 5);
-// let x = 2.5;
-// let result = Math.ceil(x) - Math.floor(x);
-// console.log(result);
-// j = 3;
-// let result2 = Math.pow(j, 2);
-// console.log(result2);
-
-
-///////////////////////////////////////////////////////////////////////////////////////// ( 2 )
-
-
-// let number = prompt("This is the box prompt , now enter the number : ");
-// alert("This is an alert box and you entered : " + number);
-
-
-///////////////////////////////////////////////////////////////////////////////////////// ( 3 )
-
-
-// let number1 =prompt("Please enter the first number :");
-// let number2 =prompt("Please enter the second number :");
-// if (number1 > number2){
-//     alert("The numbers in ascending order are: " + number1 + " , " + number2);
+// let name = prompt(" Please enter your name : ");
+// let Gender = prompt(" Please enter your gender (male or female) : ");
+// if (Gender === " male") {
+//   alert(" Hello mr " + name);
 // }
-// else if (number1 < number2) {
-//     alert("The numbers in ascending order are: " + number2 + " , " + number1);
+//  else if (Gender === "female") {
+//   alert(" Hello ms " + name);
+// } else {
+//   alert(" Welcome " + name);
 // }
-// else {
-//     alert("Both numbers are equal: " + number1);
+// let orderType = prompt(
+//   " Would you like to order Donut, Coffee, Ice cream, or Bakery "
+// );
+// let orderName = prompt(" Please write the name of the order : ");
+// alert("Your order is getting prepared  " + name);
+// console.log(
+//   " You Have one Customer " + name + " has ordered " + orderType + " ."
+// );
+/////////////////////////////////////////////////////////////////////////////(2.1)
+
+
+// let name = prompt(" Please enter your name : ");
+// let gender = prompt("Please enter your gender (male, female):");
+
+// while (gender !== "male" && gender !== "female") {
+//     gender = prompt("Please enter your gender (male, female):").toLowerCase();
 // }
 
+// if (gender === "male") {
+// alert(" Hello mr "+ name)
+//  }
 
-///////////////////////////////////////////////////////////////////////////////////////// ( 4 )
+//  else {
+//     alert(" Hello ms "+ name)
+// }
+// let orderType = prompt(" Would you like to order Donut, Coffee, Ice cream, or Bakery ");
+// let orderName = prompt(" Please write the name of the order : ");
+// alert("Your order is getting prepared  " +name);
+// console.log(" You Have one Customer " + name + " has ordered " + orderType + " .");
 
 
-// let number1 =prompt("Please enter the first number :");
-// let number2 =prompt("Please enter the second number :");
-// if (number1 > number2){
-//      alert("The larger numbers : " + number1 );
+/////////////////////////////////////////////////////////////////////////////(2.2)
+
+
+// let info = [];
+// let array = ['username', 'gender', 'type of order', 'order name'];
+
+// for (let i = 0; i < array
+// .length; i++) {
+//     let answer = '';
+    
+//     if (array
+// [i] === 'gender') {
+//         while (answer !== 'male' && answer !== 'female' ) {
+//             answer = prompt('Please enter your gender (male, female ):').toLowerCase();
+//         }
+//     } else {
+
+//         answer = prompt('Please enter your ' + array
+// [i] + ':');
+//     }
+    
+//     info.push(answer);
 // }
 
-// else if (number1 < number2) {
-//     alert("The larger numbers : "  +number2 );
+// console.log('User information:', info)
+
+/////////////////////////////////////////////////////////////////////////////(2.7)
+
+
+// for (let i = 0; i <= 5; i++) {
+//     alert(i);
 // }
-// else {
-//     alert("Both numbers are equal: " + number1);
+
+
+/////////////////////////////////////////////////////////////////////////////(2.8)
+
+
+// let num = [];
+// for (let i = 0; i <= 5; i++) {
+//     num += [i]; // num+i=num
+// }     
+// alert(num);
+
+
+/////////////////////////////////////////////////////////////////////////////(2.9)
+
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
 // }
- 
-
-///////////////////////////////////////////////////////////////////////////////////////// ( 5 )
 
 
-// let number1 = parseFloat(prompt("Please enter the first number: "));
-// let number2 = parseFloat(prompt("Please enter the second number: "));
-// let sum = number1 + number2;
-// alert("The sum of the two numbers is: " + sum);
+/////////////////////////////////////////////////////////////////////////////(2.10)
 
 
-///////////////////////////////////////////////////////////////////////////////////////// ( 6 )
 
+// let num = prompt(" Please enter a num between 0 and 100: ");
 
-// let number = prompt("Enter the number you want to convert to writing : ");
-
-// switch (parseInt(number)) {
-//   case 1:
-//     alert("ONE");
-//     break;
-//   case 2:
-//     alert("TWO");
-//     break;
-//   case 3:
-//     alert("THREE");
-//     break;
-//   case 4:
-//     alert("FOUR");
-//     break;
-//   case 5:
-//     alert("FIVE");
-//     break;
-//   case 6:
-//     alert("SIX");
-//     break;
-//   case 7:
-//     alert("SEVEN");
-//     break;
-//   case 8:
-//     alert("EIGHT");
-//     break;
-//   case 9:
-//     alert("NINE");
-//     break;
-//             default:
-//               alert("TRY AGAIN");
+// while (num < 0 || num > 100 ) {
+//     alert("Error: Please enter num between 0 and 100.");
+//     num = prompt("Please enter num between 0 and 100:");
 // }
+
+// alert("You entered: " + num);
+
+
+/////////////////////////////////////////////////////////////////////////////(2.11)
+
+
+// var num = (prompt("Please enter a num :"));
+// var sum = 0;
+// for (var i = 0; i <= num; i++) {
+//     sum += i;
+// }
+
+// alert("The sum of numbers from 0 to " + num + " is: " + sum);
+
+
+
+
+
+
