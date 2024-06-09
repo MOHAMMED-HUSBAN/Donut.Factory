79f3a033fb2f6bde69d6cd13f9ffa559ad1dbf01
mohammed-husban <22015295011@std.bau.edu.jo>
2024-06-10 01:41:44 +0300

    ol / li

--- donuts-factory.js
+++ donuts-factory.js
@@ -143,34 +143,36 @@
 
 ////////////////////////////ex4//////////////////////////////////////
 
-    let name = prompt("Please enter your name: ");
-    let gender = prompt("Please enter your gender (male, female): ").toLowerCase();
-    while (gender !== "male" && gender !== "female") {
-        gender = prompt("Please enter your gender (male, female): ").toLowerCase();
-    }
-    let orderType = prompt("Would you like to order Donut, Coffee, Ice cream, or Bakery: ");
-    let orderName = prompt("Please write the name of the order: ");
+let name = prompt("Please enter your name: ") ;
+let age = prompt("How old are you: ") ;
+let gender = prompt("Please enter your gender (male, female): ").toLowerCase();
+while (gender !== "male" && gender !== "female") {
+    gender = prompt("Please enter your gender (male, female): ").toLowerCase();
+}
+let orderType = prompt("Would you like to order Donut, Coffee, Ice cream, or Bakery: ") ;
+let orderName = prompt("Please write the name of the order: ") ;
 
-   
-    const div = document.createElement('div');
-    const paragraph = document.createElement('p');
-    const ol = document.createElement('ol');
-    const liOrderType = document.createElement('li');
-    const liOrderName = document.createElement('li');
+const div = document.createElement('div');
+const paragraph = document.createElement('p');
+const ol = document.createElement('ol');
+const liOrderType = document.createElement('li');
+const liOrderName = document.createElement('li');
+const liGender = document.createElement('li');
+const liAge = document.createElement('li');
 
+liOrderName.textContent = "Customer Name : " + name;
+liGender.textContent = "Customer Gender : " + gender;
+liAge.textContent = "Customer Age : " + age;
+liOrderType.textContent = "Order Type : " + orderType;
 
-    liOrderType.textContent = "Order Type: "+ orderType ;
-    liOrderName.textContent = "Order Name: "+ orderName;
+ol.appendChild(liOrderName);
+ol.appendChild(liGender);
+ol.appendChild(liAge);
+ol.appendChild(liOrderType);
+div.appendChild(paragraph);
+div.appendChild(ol);
 
-   
-    ol.appendChild(liOrderType);
-    ol.appendChild(liOrderName);
-    div.appendChild(paragraph);
-    div.appendChild(ol);
-
-   
-    document.getElementById("user-order").appendChild(div);
-
+document.getElementById("user-order").appendChild(div);
 
 
 
